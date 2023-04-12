b8ac1d4ac93212c6ccff48a8c798bb2b1d3fa2e5
WhiteLeafYT <117046743+WhiteLeafYT@users.noreply.github.com>
2023-04-12 17:58:17 -0400

    Add files via upload

---
+++ AstraPHP/output.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class Student{
+    var $name;
+    var $age;
+    var $gpa;
+    var $major;
+};
+$student1 = new Student();
+$student1->name = "Silas";
+$student1->age = 13;
+$student1->gpa = 4.00;
+$student1->major = "Software Engineer";
+
+
+?>
